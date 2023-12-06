b4c05c92f2f265ef136c4cf98a212c45b890b034
Fernando Santos <fernandopsilvadossantos@gmail.com>
2023-12-06 20:33:18 -0300

    Adicionando o front-end e criando o ApiRest para comunicações

--- create database.sql
+++ create database.sql
@@ -1,60 +1,34 @@
-CREATE TABLE clientes (
-	documento VARCHAR ( 14 ) PRIMARY key not null ,
-	razao_social VARCHAR ( 100 ) NOT NULL,
-	logradouro VARCHAR ( 100 ) NOT NULL,
-	bairro VARCHAR ( 50 ) NOT NULL,
-	localidade VARCHAR ( 100 ) NOT NULL,
-	uf VARCHAR ( 10 ) NULL,
-	cep VARCHAR ( 10 ) NOT NULL,
-	produto VARCHAR ( 100 ) NOT NULL
+CREATE TABLE onion.clientes (
+	documento varchar(14) NOT NULL,
+	razao_social varchar(100) NOT NULL,
+	logradouro varchar(100) NOT NULL,
+	bairro varchar(50) NOT NULL,
+	localidade varchar(100) NOT NULL,
+	uf varchar(10) NULL,
+	cep varchar(10) NOT NULL,
+	CONSTRAINT clientes_pkey PRIMARY KEY (documento)
 );
 
-
-
-CREATE TABLE onion.produtos (
-	id_produto int8 NOT NULL GENERATED ALWAYS AS identity primary key,
-	tipo_produto varchar(100) NOT NULL,
-	nome varchar(100) NOT NULL,
-	documento_cliente varchar(14) not NULL,
-	valor money not NULL
-);
-
-CREATE SEQUENCE produtos_id_produto_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 9223372036854775807
-	START 1
-	CACHE 1
-	NO CYCLE;
-
-
-ALTER TABLE onion.produtos ADD CONSTRAINT produtos_documento_cliente_fkey FOREIGN KEY (documento_cliente) REFERENCES onion.clientes(documento);
 
 CREATE TABLE onion.pedidos (
-	id_pedido int8 NOT NULL GENERATED ALWAYS AS identity primary key,
-	id_produto int8 not null,
+	id_pedido int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
+	id_produto int8 NOT NULL,
 	numero_pedido int4 NOT NULL,
 	data_entrega date NOT NULL,
-	frete money NOT NULL
+	frete money NOT NULL,
+	CONSTRAINT pedidos_pkey PRIMARY KEY (id_pedido)
 );
-
-CREATE SEQUENCE pedidos_id_pedido_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 9223372036854775807
-	START 1
-	CACHE 1
-	NO CYCLE;
 
 ALTER TABLE onion.pedidos ADD CONSTRAINT produtos_id_produto_fkey FOREIGN KEY (id_produto) REFERENCES onion.produtos(id_produto);
 
 
+CREATE TABLE onion.produtos (
+	id_produto int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
+	tipo_produto varchar(100) NOT NULL,
+	nome varchar(100) NOT NULL,
+	documento_cliente varchar(14) NOT NULL,
+	valor money NOT NULL,
+	CONSTRAINT produtos_pkey PRIMARY KEY (id_produto)
+);
 
-
-
-
-
-ADD COLUMN documento_cliente varchar(14) REFERENCES clientes(documento);
-
-ALTER TABLE pedidos
-ADD COLUMN id_produto bigint REFERENCES produtos(id_produto);
+ALTER TABLE onion.produtos ADD CONSTRAINT produtos_documento_cliente_fkey FOREIGN KEY (documento_cliente) REFERENCES onion.clientes(documento);
